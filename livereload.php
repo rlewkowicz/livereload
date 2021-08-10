4bc56866fc2f9df3be79e5d3a1f452ba1f9655ea
Ryan Lewkowicz <ryan.m.lewkowicz@gmail.com>
2021-08-10 07:19:07 -0400

    init

---
+++ livereload.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: LiveReload
+Plugin URI: https://hireryan.today
+Description: Add the LiveReload javascript snippet to your site
+Version: 1.0
+Author: Ryan Lewkowicz
+Author URI: https://github.com/rlewkowicz
+*/
+
+function livereload_snippet_enqueue_js() {
+    wp_register_script('livereload', 'http://localhost:35729/livereload.js?snipver=1', null, false, true);
+    wp_enqueue_script('livereload');
+}
+
+add_action( 'wp_enqueue_scripts', 'livereload_snippet_enqueue_js' );
+add_action( 'admin_enqueue_scripts', 'livereload_snippet_enqueue_js' );
